aa17664d3c24e14af3e7199351868def2a0d9c2b
tnsgud9 <tnsgud9@naver.com>
2021-11-30 21:56:03 +0900

    Update Carrot main menu design

--- src/App.tsx
+++ src/App.tsx
@@ -1,70 +1,52 @@
-import React from "react";
-import logo from "./logo.svg";
-import "./App.css";
-import Button from "./Button";
-import Title from "./Title";
-import Text from "./Text";
-import Adult from "./Adult";
-import Photo from "./Photo";
-import { ChangeEvent } from "react";
-import { KeyboardEvent } from "react";
-import Counter from "./Counter";
-import Adder from "./Adder";
-import Welcome from "./Welcome";
-import ConditionalText from "./ConditionalText";
-import HiddenName from "./HiddenName";
-import { useState } from "react";
 import Test from "./pages/Test";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import { Box, Button, Grid } from "@mui/material";
 
 const App = () => {
-  const [lastestName, setLastestName] = useState("");
-  const updateLastestName = (name: string) => {
-    setLastestName(name);
-  };
-
-  const count = 1;
-  const welcomeText = "Title!";
-  const welcomeText02 = "subtitle!";
-  const values = "hahahahahahah!!!";
-  const realtextType = "strong";
-  const myAge = 25;
-  const imgsrc =
-    "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRe2QVOaV5TRtyjv1L1uprcgkrKeebh453KcG8OX0D7zqvr5puREbt0_5DciT61l2XxjuU&usqp=CAU";
-  const articlesrc = "잘가라 이순형";
-
-  const preventKeyDown = (event: KeyboardEvent<HTMLInputElement>) => {
-    console.log(event.key);
-    if (event.key === "h") {
-      event.preventDefault();
-      event.stopPropagation();
-    }
-  };
-
   return (
-    <>
-      <Test />
-
-      {/*<input type="text" onKeyDown={preventKeyDown} />
-      <Counter defaultCount={0} />
-      <Adder />
-      <Welcome />
-      <ConditionalText />
-      <HiddenName />
-      <Button name="hello" callback={updateLastestName} />
-      <Button name="world" callback={updateLastestName} />
-      <Button name="nextop" callback={updateLastestName} />
-  <article>{lastestName} is clicked!</article>*/}
-
-      {/*s
-    <div className="App">
-      <Title title={welcomeText} />
-      <Text myText={values} myType={realtextType} />
-      {count > 5 ? <Button name="go" /> : <Button name="back" />}
-      <Adult age={myAge} />
-      <Photo address={imgsrc} description={articlesrc} />
-    </div>
-    */}
-    </>
+    <main>
+      <AppBar position="fixed">
+        <Toolbar></Toolbar>
+      </AppBar>
+      <Toolbar />
+      <Grid container>
+        내용
+        <Test />
+      </Grid>
+      <AppBar position="fixed" sx={{ top: "auto", bottom: 0 }}>
+        <Toolbar>
+          <Grid container>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                <Button>홈</Button>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                <Button>동네생활</Button>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                <Button>내 근처</Button>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                <Button>채팅</Button>
+              </Box>
+            </Grid>
+            <Grid item xs>
+              <Box display="flex" justifyContent="center">
+                <Button>나의 당근</Button>
+              </Box>
+            </Grid>
+          </Grid>
+        </Toolbar>
+      </AppBar>
+    </main>
   );
 };
 
--- src/pages/Test/index.tsx
+++ src/pages/Test/index.tsx
@@ -4,29 +4,27 @@ import { Container, Grid } from "@mui/material";
 const Test = (): JSX.Element => {
   return (
     <Container style={{ backgroundColor: "#022fff" }}>
-      <Grid container style={{ backgroundColor: "ff2fff" }}>
-        <Grid item xs={12}>
+      <Grid container style={{ backgroundColor: "#222fff" }}>
+        <Grid item xs={12} md={6}>
+          xs-12
+        </Grid>
+        <Grid item xs={12} md={6}>
           xs-12
         </Grid>
       </Grid>
-      <Grid container style={{ backgroundColor: "ff9fff" }}>
-        <Grid item xs={6} style={{ backgroundColor: "ffff2f" }}>
-          xs-6
-        </Grid>
-        <Grid item xs={6}>
-          xs-6
-        </Grid>
-      </Grid>
 
-      <Grid container style={{ backgroundColor: "ff9fff" }}>
-        <Grid container style={{ backgroundColor: "ffff2f" }}>
-          xs-auto
+      <Grid container style={{ backgroundColor: "#ff9fff" }}>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
         </Grid>
-        <Grid item xs={4}>
-          xs-4
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
         </Grid>
-        <Grid item xs style={{ backgroundColor: "ffff2f" }}>
-          xs-auto
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
+        </Grid>
+        <Grid item xs={6} md={3} style={{ backgroundColor: "#ffff2f" }}>
+          xs-6
         </Grid>
       </Grid>
     </Container>
